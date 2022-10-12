991925593490bff5b3d98bf08bbde9f348ddfbc8
dLeczycki <danielleczycki@gmail.com>
2022-10-12 10:38:36 +0200

    Move main to global directory

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { config } from '../config/config';
-import { GlobalExceptionFilter } from '../filters/global-exception.filter';
+
+import { AppModule } from './app/app.module';
+import { config } from './config/config';
+import { GlobalExceptionFilter } from './filters/global-exception.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new GlobalExceptionFilter());
+
   await app.listen(config.port);
 }
 
